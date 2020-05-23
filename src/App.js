0356eb3bce7dcae3ba09071cccc586bdccbb6a60
Eduardo Daniel Sanchez <eduardo.daniel.s.m@gmail.com>
2020-05-23 00:38:06 -0400

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import GamePage from './pages/GamePage';
 import HomePage from './pages/HomePage';
 import AppNavigator from './components/AppNavigator';
 
-
 const App = () => {
     return (
       <>
