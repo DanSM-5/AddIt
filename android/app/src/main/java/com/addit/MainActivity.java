5ac5f391dc27d01aa0e7ff4df76fc7a3eb868f61
daniel <eduardo.daniel.s.m@gmail.com>
2023-07-24 21:54:29 -0400

    fix: Use correct package name

--- android/app/src/main/java/com/addit/MainActivity.java
+++ android/app/src/main/java/com/addit/MainActivity.java
@@ -1,4 +1,4 @@
-package com.addit;
+package com.eduardosanchez.addit;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/addit/MainApplication.java
+++ android/app/src/main/java/com/addit/MainApplication.java
@@ -1,4 +1,4 @@
-package com.addit;
+package com.eduardosanchez.addit;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
