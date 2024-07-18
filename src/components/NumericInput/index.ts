f4a6e6ef63affbdb29004304ae3218dd505694fb
daniel <eduardo.daniel.s.m@gmail.com>
2024-07-18 17:24:42 -0400

    refactor: Remove comment

--- src/components/NumericInput/index.ts
+++ src/components/NumericInput/index.ts
@@ -1,4 +1,3 @@
 // Copied from https://github.com/himelbrand/react-native-numeric-input
 export * from './Button';
-// NOTE: This is just the Android version.
 export { default as NumericInput } from './NumericInput';
