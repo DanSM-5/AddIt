7b5de8e5f972e1c129000303b909cd533b7c29f3
daniel <eduardo.daniel.s.m@gmail.com>
2024-07-18 12:24:20 -0400

    feat: Add NumericInput package to repo directly
    NOTE: This only applies to android version of the original package

---
+++ src/components/NumericInput/index.ts
@@ -0,0 +1,4 @@
+// Copied from https://github.com/himelbrand/react-native-numeric-input
+export * from './Button';
+// NOTE: This is just the Android version.
+export { default as NumericInput } from './NumericInput';
